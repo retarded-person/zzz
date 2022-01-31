912a87742211bdbfc842227b683a21741fc2657a
test <73199280+retarded-person@users.noreply.github.com>
2022-01-31 04:25:58 -0800

    Delete fake-useragent directory

--- fake-useragent/tests/test_errors.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import, unicode_literals
-
-from fake_useragent import errors
-
-
-def test_error_aliases():
-    assert errors.FakeUserAgentError is errors.UserAgentError
